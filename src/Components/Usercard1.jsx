919ed6c681b7242d3c9269225a0a7772bbb8f79e
codesak <sakshamverma7844@gmail.com>
2022-11-18 18:44:20 +0530

    ok

--- src/Components/Usercard1.jsx
+++ src/Components/Usercard1.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Card } from "antd";
 import { useState, useEffect } from 'react';
 import "./UserCard2.css"
+import { Modal } from 'antd';
+
 
 
 import {
@@ -18,8 +20,19 @@ import {
 import axios from 'axios';
 const { Meta } = Card;
 
-export default function Usercard1({ res3 ,index ,setuserId,userId}) {
+export default function Usercard1({ res3, index, setuserId, userId }) {
     const [avatar, setAvatar] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
 
 
 
@@ -28,8 +41,10 @@ export default function Usercard1({ res3 ,index ,setuserId,userId}) {
         axios.get(`https://avatars.dicebear.com/v2/avataaars/${res3?.name}.svg?options[mood][]=happy`).then((res) => {
             setAvatar(res?.data);
         });
-    },[]);
+    }, []);
     const [lovebutton, setLovebutton] = useState(false)
+
+
     return (
 
 
@@ -39,20 +54,34 @@ export default function Usercard1({ res3 ,index ,setuserId,userId}) {
                 flexBasis: "22%",
                 margin: "11px",
                 height: "fit-content",
-                alignItems:"center",
-                minWidth:"280px",
-                
+                alignItems: "center",
+                minWidth: "280px",
+
             }}
             cover={<img alt="example" style={{ height: "200px", backgroundColor: "#f5f5f5" }} src={`https://avatars.dicebear.com/v2/avataaars/${res3?.name}.svg?options[mood][]=happy`} />}
             actions={[
-                lovebutton ? <HeartFilled style={{color:"red"}} onClick={()=>setLovebutton(!lovebutton)}/> : <HeartOutlined style={{ color: "red" }} onClick={()=>setLovebutton(!lovebutton)} />,
-                <EditOutlined />,
-                <DeleteOutlined  onClick={()=>{
-                    setuserId([...userId.slice(0,index),...userId.slice(index+1,userId?.length)])
-                    console.log([...userId.slice(index+1,userId?.length)]);
+                lovebutton ? <HeartFilled style={{ color: "red" }} onClick={() => setLovebutton(!lovebutton)} /> : <HeartOutlined style={{ color: "red" }} onClick={() => setLovebutton(!lovebutton)} />,
+
+                <EditOutlined onClick={showModal}
+                />,
+                <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                    <div style={{ display: "flex", flexDirection: "column", width:"fit-content" }}>
+                        <p style={{width:"fit-content"}}>Name: <input type="text" defaultValue={res3.name} style={{
+                            opacity: ".56", height: "30px", padding: " 4px 11px", borderRadius: "4px"
+                        }} /></p>
+
+                        <p style={{width:"fit-content"}}> Email: <input type="text" defaultValue={res3.email} style={{ opacity: ".56", width: "80%", height: "30px", padding: " 4px 11px", borderRadius: "4px" }} /></p>
+                        <p style={{width:"fit-content"}}>Phone: <input type="text" defaultValue={res3.phone} style={{ opacity: ".56", width: "80%", height: "30px", padding: " 4px 11px", borderRadius: "4px" }} /></p>
+                        <p style={{width:"60%"}}>Address: <input type="text" defaultValue={res3.address.street + "" + res3.address.city} style={{ opacity: ".56", width: "50%" , height: "30px", padding: " 4px 11px", borderRadius: "4px"}} /></p>
+                        <p style={{width:"fit-content"}}>Website: <input type="text" defaultValue={res3.website} style={{ opacity: ".56", width: "80%" , height: "30px", padding: " 4px 11px", borderRadius: "4px"}} /></p>
+                        <p style={{width:"fit-content"}}>Company Name: <input type="text" defaultValue={res3.company.name} style={{ opacity: ".56", width: "80%" , height: "30px", padding: " 4px 11px", borderRadius: "4px"}} /></p></div>
+                </Modal>,
+                <DeleteOutlined onClick={() => {
+                    setuserId([...userId.slice(0, index), ...userId.slice(index + 1, userId?.length)])
+                    console.log([...userId.slice(index + 1, userId?.length)]);
                     console.log(userId?.length);
-                    console.log([...userId.slice(index,userId?.length)]);
-                }}/>,
+                    console.log([...userId.slice(index, userId?.length)]);
+                }} />,
             ]}
         >
             <Meta title={res3.name} />
@@ -61,7 +90,7 @@ export default function Usercard1({ res3 ,index ,setuserId,userId}) {
                 avatar={<MailOutlined />}
                 description={res3.email}
             />
-            <img src={avatar} alt="" />
+
             <Meta
                 style={{ opacity: ".55" }}
                 avatar={<PhoneOutlined />}
